8e2ff40e14e10e0d73e629980b82bb85f292bd99
faranaandrade <farana@occ.com.mx>
2024-01-15 17:27:26 -0600

    se hace los cambios para hacer que se adapten a los datos de resume proporcioandos por el mys en este campo

--- internal/models/message_to_process.go
+++ internal/models/message_to_process.go
@@ -2,9 +2,13 @@ package models
 
 // MessageToProcess : mensaje a procesar
 type MessageToProcess struct {
-	RetryNumber  int    `json:"retryNumber"` // Iteración actual de reprocesamiento en el tópico actual (main topic)
-	UserID       string `json:"userid"`
-	JobID        int    `json:"jobId"`
-	CreationDate string `json:"creationDate"`
-	ResumeID     int    `json:"resumeId"`
+	RetryNumber       int    `json:"retryNumber"` // Iteración actual de reprocesamiento en el tópico actual (main topic)
+	UserID            string `json:"userid"`
+	JobID             int    `json:"jobId"`
+	CreationDate      string `json:"creationDate"`
+	ResumeID          int    `json:"resumeId"`
+	CvReady           bool   `json:"cv_ready"`
+	CvAttached        bool   `json:"cv_attached"`
+	EducationLevel    string `json:"education_level"`
+	YearsOfExperience int    `json:"years_of_experience"`
 }
